2a8cdb8f088a7fb0f0e747c1ed389c4cb7ab73a6
Simon PARIS <ps205947@etu.unice.fr>
2015-04-16 18:22:40 +0200

    mise jour outils

--- refactor/refactor.py
+++ refactor/refactor.py
@@ -10,6 +10,7 @@ import json
 import sys
 
 line = 1
+exploitRess = ""
 
 def handleLand(dico):
     print(line, dico["data"]["action"] , file=out_file, end="(")
@@ -46,9 +47,28 @@ def handleMove_to(dico):
     print(line, dico["data"]["action"] , file=out_file, end="(")
     print(dico["data"]["parameters"]["direction"] + ")" , file=out_file)
 
-def handleExploit(dico):
+
+def handleExploit_Explorer(dico):
     print(line, dico["data"]["action"] , file=out_file, end="(")
     print(dico["data"]["parameters"]["resource"] + ")" , file=out_file)
+
+
+def handleExploit_Engine(dico):
+	if exploitRess == resource[0] :
+		global av1
+		avancement[0] = avancement[0] + dico["data"]["extras"]["amount"]
+	elif exploitRess == resource[1] :
+		global av2
+		avancement[1] = avancement[1] + dico["data"]["extras"]["amount"]
+	elif exploitRess == resource[2] :
+		global av3
+		avancement[2] = avancement[2] + dico["data"]["extras"]["amount"]
+	elif exploitRess == resource[3] :
+		global av4
+		avancement[3] = avancement[3] + dico["data"]["extras"]["amount"]
+	elif exploitRess == resource[4] :
+		global av5
+		avancement[4] = avancement[4] + dico["data"]["extras"]["amount"]
 
 
 def handleScout_Explorer(dico):
@@ -63,6 +83,10 @@ def handleScout_Engine(dico):
 	print("-)" ,file=out_file)
 
 
+resource = [0]*5
+objectif = [0]*5
+avancement = [0]*5
+
 
 # main --------------------------
 in_file = open(sys.argv[1], "r")
@@ -72,6 +96,32 @@ out_file = open(sys.argv[2], "w")
 json_dict = json.load(in_file)
 
 for info in json_dict:
+	#init
+	if "data" in info and "objective" in info["data"]:
+		i = 0
+		while i < len(info["data"]["objective"]):
+			if i == 0:
+				resource[0] = info["data"]["objective"][i]["resource"]
+				objectif[0] = info["data"]["objective"][i]["amount"]
+				i = i + 1
+			elif i == 1:
+				resource[1] = info["data"]["objective"][i]["resource"]
+				objectif[1] = info["data"]["objective"][i]["amount"]
+				i = i + 1
+			elif i == 2:
+				resource[2] = info["data"]["objective"][i]["resource"]
+				objectif[2] = info["data"]["objective"][i]["amount"]
+				i = i + 1
+			elif i == 3:
+				resource[3] = info["data"]["objective"][i]["resource"]
+				objectif[3] = info["data"]["objective"][i]["amount"]
+				i = i + 1
+			elif i == 4:
+				resource[4] = info["data"]["objective"][i]["resource"]
+				objectif[4] = info["data"]["objective"][i]["amount"]
+				i = i + 1
+			
+
 	# part: EXPLORER
 	if "data" in info and "action" in info["data"]: 
 		#action : LAND
@@ -92,14 +142,18 @@ for info in json_dict:
 			line = line + 1
 		#action : EXPLOIT
 		if info["data"]["action"] == "exploit":
-			handleExploit(info)
+			handleExploit_Explorer(info)
 			line = line + 1
+			exploitRess	= info["data"]["parameters"]["resource"]
 		#action : SCOUT
 		if info["data"]["action"] == "scout":
 			handleScout_Explorer(info)
 			line = line + 1
 
 
+	# part: EXPLORE_EXPLOIT
+	if ("data" in info and "extras" in info["data"] and "amount" in info["data"]["extras"]):
+		handleExploit_Engine(info)
 	# part: EXPLORE_ENGINE
 	if ("data" in info and "extras" in info["data"] and "resources" in info["data"]["extras"] and not("altitude" in info["data"]["extras"])):
 		handleExplore_Engine(info)
@@ -109,4 +163,6 @@ for info in json_dict:
 	# part: GLIMPSE_ENGINE
 	if ("data" in info and "extras" in info["data"] and "report" in info["data"]["extras"]):
 		handleGlimpse_Engine(info)
+
+print(resource[0], avancement[0], "/", objectif[1], "---", resource[1], avancement[1], "/", objectif[1], "---", resource[2], avancement[2], "/", objectif[2], "---", resource[3], avancement[3], "/", objectif[3], "---", resource[4], avancement[4], "/", objectif[4], "---", file=out_file)
 
